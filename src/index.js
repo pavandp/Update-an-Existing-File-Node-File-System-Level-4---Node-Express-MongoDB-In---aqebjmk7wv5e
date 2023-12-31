70b1d546ac4b08899af2ed7ca9860163d3c65f78
pavankdp <pavankdp29@gmail.com>
2023-12-31 19:31:37 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ const fileName = "myfile.txt";
 const updateFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
-  
+  const existing=await fs.readFile(fileName, 'utf-8');
+  const content=`${existing}, ${fileContent}`;
+  await fs.writeFile(fileName, content, 'utf-8');
 };
 
 module.exports = updateFile;
